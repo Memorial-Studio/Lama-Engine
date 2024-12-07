b401ce53b1153c2dd96deccef27e095a6113e660
Explorer <russiatop8902@gmail.com>
2024-12-07 19:05:45 +0800

    Added event handling

--- LECore/includes/LECore/Application.h
+++ LECore/includes/LECore/Application.h
@@ -1,11 +1,12 @@
 //
 // Created by Ganza on 05.12.2024.
 // 
-// Changed by Jack Daniels on 06.12.2024.
+// Changed by Jack Daniels on 07.12.2024.
 // 
 #pragma once
 
 #include <memory>
+#include "LECore/Event.h"
 
 namespace LamaEngine
 {
@@ -27,5 +28,7 @@ namespace LamaEngine
 	private:
 		std::unique_ptr<class Window> m_pWindow;
 
+		EventDispatcher m_event_dispatcher;
+		bool m_bCloseWindow = false;
 	};
 }
